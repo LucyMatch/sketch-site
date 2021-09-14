1969cd6f5c37f658a809c7d00ac2e441f06cf3b9
Lucy Matchett <lucy.matchett@gmail.com>
2021-09-14 14:59:56 -0400

    integrated new data format - sketch page now retrieves sketch file - used a hook to append script to dom

--- src/client/components/Sketch.js
+++ src/client/components/Sketch.js
@@ -1,19 +1,32 @@
 import React, { useState, useEffect} from 'react'
 import Container from 'react-bootstrap/Container'
-import ScriptTag from 'react-script-tag'
-import { useParams } from 'react-router-dom';
+// import ScriptTag from 'react-script-tag'
+import useScript from '../hooks/useScript';
+import PropTypes from 'prop-types'
 
-//@TODO: retrieves sketch id from router 
-//       uses that to load teh correct script
+const Sketch = ({s}) => {
 
+    if(Object.entries(s).length > 0 ){
+        useScript('https://cdn.jsdelivr.net/npm/p5@1.4.0/lib/p5.js');
+        useScript('../sketches/'+ s.file +'.js');
+    }
 
-
-const Sketch = ({sketches}) => {
     return(
         <Container className="page-container sketch">
-            <ScriptTag type='text/javascript' src='../sketches-test/test.js' />
+            { Object.entries(s).length > 0 ? (
+                <div>
+                    {/* if yes script found... */}
+                </div>
+            ) : (
+                <h2 className="secondary-title">Unable To Locate Sketch</h2>
+            ) }
+            
         </Container>
     )
+}
+
+Sketch.propTypes = {
+    s : PropTypes.object.isRequired
 }
 
 export default Sketch
--- src/client/containers/SketchPage.js
+++ src/client/containers/SketchPage.js
@@ -2,11 +2,15 @@ import { connect } from 'react-redux'
 import Sketch from '../components/Sketch'
 
 const getSketch = (sketches, p) => {
-    console.log(p);
+    var s = {}
+    Object.entries(sketches).map(([key, value]) => {
+        if( key == p.match.params.id) s = value
+    })
+    return s
 }
 
 const mapStateToProps = (state, ownProps) => ({
-    sketch: getSketch(state.app.sketches, ownProps)
+    s: getSketch(state.app.sketches, ownProps)
 })
 
 export default connect(mapStateToProps)(Sketch)
